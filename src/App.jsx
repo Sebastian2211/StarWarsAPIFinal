938f0a8b7c8ccf41c0f409c4cb8929c287adcb65
Sebastian2211 <118332249+Sebastian2211@users.noreply.github.com>
2023-07-15 02:26:36 -0700

    started add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-// {import.meta.env.VITE_VARIABLE_NAME} is how you access environment variables
-
-
 function App() {
+  const [image, setImage] = useState(true);
+
+  function clickHandler() {
+    setImage(!image);
+  }
 
   return (
-    <div>
+    <div className='container'>
+      <div className='header-title'>
+        <h1>Where are you Xur?</h1>
+      </div>
 
-      <h1>Where are you Xur?</h1>
+      <div className="Xur">
+        {image && (
+          <img src='https://www.bungie.net/img/destiny_content/pgcr/conceptual_xur.jpg' alt='Xur' className='Xur' />
+        )}
+      </div>
 
+      <button onClick={clickHandler}>Show me where he is today</button>
     </div>
-  )
+  );
 }
 
 export default App;
