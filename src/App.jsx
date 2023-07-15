c8cb783479b1a3469420607f5a5ff8bffd171111
Sebastian2211 <118332249+Sebastian2211@users.noreply.github.com>
2023-07-15 01:25:24 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+
+// {import.meta.env.VITE_VARIABLE_NAME} is how you access environment variables
+
+
+function App() {
+
+  return (
+    <div>
+
+      <h1>Where are you Xur?</h1>
+
+    </div>
+  )
+}
+
+export default App;
